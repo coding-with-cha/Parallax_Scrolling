9c2dadfe46f76df4b2df866a30e72e051347ca2a
chaima <chaimamejbri@gmail.com>
2022-03-08 17:13:11 +0100

    fCom

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './style/style.css'
+
+function App() {
+  return (
+    <div className="App">
+     <div className="parallax">
+       <img src="https://aeoneal.com/imagery/brain-reverse-cutout.svg"/>
+       </div>
+       <div className="after">
+         <h1>PARALLAX EFFECT</h1>
+         </div>
+    </div>
+  );
+}
+
+export default App;
